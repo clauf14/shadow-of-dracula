4d921116222a0f332d9b782ff0429185ca7278ee
clauf14 <claudiufurtea@gmail.com>
2025-04-07 21:13:13 +0300

    added a little animation screen

---
+++ CinematicScenes/camera_settings.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+func _ready():
+	if not $AnimationPlayer.is_connected("animation_finished", Callable(self, "_on_animation_player_animation_finished")):
+		$AnimationPlayer.connect("animation_finished", Callable(self, "_on_animation_player_animation_finished"));
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		$AnimationPlayer.play("scene");
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "scene":
+		get_tree().change_scene_to_file("res://Scenes/MainScene.tscn");
--- HomeScene/HomeScene.gd
+++ HomeScene/HomeScene.gd
@@ -1,6 +1,6 @@
 extends Control
 func _on_PlayButton_pressed():
-	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn") # Replace with your 3D game scene path
+	get_tree().change_scene_to_file("res://CinematicScenes/FirstCinematic.tscn") # Replace with your 3D game scene path
 
 func _on_LoadGameButton_pressed():
 	get_tree().change_scene_to_file("res://LoadGameScene/LoadGameScene.tscn")

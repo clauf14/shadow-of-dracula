d69211e33557e0954cc1ecbcf792b466e9f6fea9
clauf14 <claudiufurtea@gmail.com>
2025-04-11 21:51:14 +0300

    terrain with Terrain3d addon

--- HomeScene/HomeScene.gd
+++ HomeScene/HomeScene.gd
@@ -1,5 +1,8 @@
 extends Control
 func _on_PlayButton_pressed():
+	#go to 3d terrain
+	#get_tree().change_scene_to_file("res://WorldTerrain.tscn")
+	#go to cinematic
 	get_tree().change_scene_to_file("res://CinematicScenes/FirstCinematic.tscn") # Replace with your 3D game scene path
 
 func _on_LoadGameButton_pressed():
